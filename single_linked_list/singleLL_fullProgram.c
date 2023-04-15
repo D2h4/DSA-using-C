213fe38f73937d72368f9acccfae576cf798885e
Aman Dhiman <109021534+D2h4@users.noreply.github.com>
2023-04-15 14:41:21 +0530

    Add files via upload

---
+++ single_linked_list/singleLL_fullProgram.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+typedef struct nodetype
+{
+  int info;
+  struct node *next;
+}node;
+void main()
+{   node *head=NULL;
+    int choice,item;
+    while(1)
+    {   printf("enter your choice: ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+          case 1:
+              printf("enter the item: ");
+              scanf("%d",&item);
+                insertionFromBeg(&head,item);
+              break;
+          case 2:
+             traversal(head);
+             break;
+          case 3:
+             deletionFromBeg(&head);
+             break;
+          case 4:
+              printf("enter item: ");
+              scanf("%d",&item);
+             insertionFromEnd(&head,item);
+             break;
+          case 5:
+            deletionFromEnd(&head);
+            break;
+          default:
+            exit(0);
+        }
+    }
+}
+void insertionFromBeg(node **head,int item)
+{
+    node *ptr;
+    ptr=(node*)malloc(sizeof(node));
+    ptr->info=item;
+    if(*head==NULL)
+    {
+        ptr->next=NULL;
+    }
+    else
+    {
+        ptr->next=*head;
+    }
+    *head=ptr;
+}
+void deletionFromBeg(node** head)
+{
+      node *loc;
+      loc=*head;
+      (*head)=(*head)->next;
+      free(loc);
+}
+void traversal(node *head)
+{
+    while(head!=NULL)
+    {
+        printf("%d\t",head->info);
+        head=head->next;
+    }
+}
+void insertionFromEnd(node **head,int item)
+{
+    node *ptr;
+    ptr=(node*)malloc(sizeof(node));
+    ptr->info=item;
+    ptr->next=NULL;
+    if(*head==NULL)
+    {
+        *head=ptr;
+    }
+    else
+    {
+         node *loc;
+         loc=*head;
+        while(loc->next!=NULL)
+        {
+            loc=loc->next;
+        }
+        loc->next=ptr;
+    }
+}
+void deletionFromEnd(node **head)
+{
+    node *ptr,*loc;
+    ptr=*head;
+    while(ptr->next!=NULL)
+    {   loc=ptr;
+        ptr=ptr->next;
+
+    }
+    loc->next=NULL;
+    free(ptr);
+}
